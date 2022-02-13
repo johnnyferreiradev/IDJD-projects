c39979a256d413151db7d84271898a8bc6095439
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2022-02-12 23:53:59 -0300

    Removendo cenas não usadas

--- freeway/Main.gd
+++ freeway/Main.gd
@@ -6,7 +6,6 @@ var slowRoad= [600, 544, 438, 324, 384, 216, 160]
 var fastRoad = [488, 272, 104]
 
 var score1 = 0
-var score2 = 0
 
 func _ready():
 	$restartButton.hide()
@@ -38,12 +37,11 @@ func _on_Player_toScore():
 	if score1 >= 3:
 		$restartButton.show()
 		$theme.stop()
-		$result.text = "Player 1 venceu!"
+		$result.text = "Você venceu!"
 		$youWin.play()
 		$slowCarTimer.stop()
 		$fastCarTimer.stop()
 		$Player.setBlockMovement(true)
-		$Player2.setBlockMovement(true)
 
 
 func _on_restartButton_pressed():
